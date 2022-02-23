81b3e684653c26a3486774e703ee6107eddeb91b
Elegye <alexandrehalope@yahoo.fr>
2022-02-23 23:32:24 +0100

    :zap: Remove file saving to disk

--- get_flights.py
+++ get_flights.py
@@ -21,35 +21,29 @@ flights = fr_api.get_flights(bounds=bounds)
 filename = "data/{}.json".format(datetime.datetime.now().timestamp())
 Path("./data").mkdir(parents=True, exist_ok=True)
 
-with open(filename, "w") as file:
-    json.dump(flights, file, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-
 with InfluxDBClient(url='http://192.168.1.38:8086', token=f'{username}:{password}', org='-') as client:
     with client.write_api() as write_api:
         print('*** Write Points ***')
-        with open(filename, "r") as file:
-            flights = json.load(file)
+        for flight in flights:
+            point = Point("position")
+            point = point.tag("aircraft_code", flight.aircraft_code)
+            point = point.tag("airline_iata", flight.airline_iata)
+            point = point.tag("origin_airport_iata", flight.origin_airport_iata)
+            point = point.tag("destination_airport_iata", flight.destination_airport_iata)
+            point = point.tag("callsign", flight.callsign)
+            point = point.tag("number", flight.number)
+            point = point.tag("registration", flight.registration)
+            point = point.tag("id", flight.id)
 
-            for flight in flights:
-                point = Point("position")
-                point = point.tag("aircraft_code", flight["aircraft_code"])
-                point = point.tag("airline_iata", flight["airline_iata"])
-                point = point.tag("origin_airport_iata", flight["origin_airport_iata"])
-                point = point.tag("destination_airport_iata", flight["destination_airport_iata"])
-                point = point.tag("callsign", flight["callsign"])
-                point = point.tag("number", flight["number"])
-                point = point.tag("registration", flight["registration"])
-                point = point.tag("id", flight["id"])
-
-                point = point.field("altitude", flight["altitude"])
-                point = point.field("ground_speed", flight["ground_speed"])
-                point = point.field("heading", flight["heading"])
-                point = point.field("latitude", flight["latitude"])
-                point = point.field("longitude", flight["longitude"])
-                point = point.field("vertical_speed", flight["vertical_speed"])
+            point = point.field("altitude", flight.altitude)
+            point = point.field("ground_speed", flight.ground_speed)
+            point = point.field("heading", flight.heading)
+            point = point.field("latitude", flight.latitude)
+            point = point.field("longitude", flight.longitude)
+            point = point.field("vertical_speed", flight.vertical_speed)
                 
-                point = point.time(flight["time"] * 1000, write_precision=WritePrecision.MS)
+            point = point.time(flight.time * 1000, write_precision=WritePrecision.MS)
 
-                print(point.to_line_protocol())
+            print(point.to_line_protocol())
 
-                write_api.write(bucket=bucket, record=point)
+            write_api.write(bucket=bucket, record=point)
